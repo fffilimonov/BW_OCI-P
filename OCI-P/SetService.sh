330c1f54052e61ef8489893a4cde1bc2a2f4afdd
fffilimonov <fffilimonov@yandex.ru>
2015-02-18 11:28:49 +0300

    added setservice

---
+++ OCI-P/SetService.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage $0 USERID";
+    exit 1;
+fi;
+
+. config;
+. common;
+command="xml/services1.xml.tmpl";
+
+USERID=$1;
+
+export USERID;
+./lib/OCIclient.sh $command;
